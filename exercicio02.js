928dc7644a282be881d0bfc69f2a84eb552a12e4
EmiliaPaula <emiliaand_paula1@msn.com>
2022-05-26 02:36:57 -0300

    Exercicio 2: Conta de Restaurante (calculos com variáveis)

---
+++ exercicio02.js
@@ -0,0 +1,18 @@
+// Neste exercício vamos aprender como realizar cálculos com variáveis e template literals.
+
+// EXERCÍCIO 2 
+// Elaborar um programa que leia o valor de um jantar. Calcule e informe o valor da taxa do garçom (10%) e o valor total a ser pago.
+// EXEMPLO: 
+// Valor do jantar: R$80,00
+// Taxa do garçom: R$8,00
+// Total a pagar: R$88,00
+// ---------------------------------------------------------------
+
+let valorJantar = 80;
+let taxaGarcom = 10;
+let totalPagar = 88;
+
+total = 80 + ((taxaGarcom / 100) * 80);
+console.log('Valor do jantar: R$' + valorJantar + ',00');
+console.log('Taxa do garçom: R$' + ((taxaGarcom / 100) *80) + ',00');
+console.log('Total a pagar: R$' + total + ',00')
